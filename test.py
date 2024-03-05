d589855276ede4b8023a728611ebaf0ed9f67b25
SangWon7242 <gotkdqja@gmail.com>
2024-03-05 20:50:40 +0900

    find, find_all을 이용한 html 요소 검색

--- test.py
+++ test.py
@@ -11,24 +11,36 @@ html = """
 <nav class="menu-box-1" id="menu-box">
   <ul>
     <li>
-      <a href="https://www.naver.com">네이버로 이동</a>
+      <a class="menu-item-text" href="https://www.naver.com">네이버로 이동</a>
     </li>
     <li>
-      <a href="https://www.google.com">구글로 이동</a>
+      <a class="menu-item-text" href="https://www.google.com">구글로 이동</a>
     </li>
     <li>
-      <a href="https://www.daum.net">다음으로 이동</a>
+      <a class="menu-item-text" href="https://www.daum.net">다음으로 이동</a>
     </li>
   </ul>
 </nav>
+<div id="box"></div>
 """
 
 # HTML 파싱
 bs = BeautifulSoup(html, 'html.parser')
 
-a_tags = bs.select('a')
-a_tag = bs.select_one('a')
-print(a_tag)
+# find_all : 조건과 일치하는 모든 요소의 리스트
+# 해당 엘리먼트의 클래스 이름으로 접근
+menu_item_text = bs.find_all('a', class_="menu-item-text")
 
-# for tags in a_tags:
-#   print(tags.get_text())
+'''
+for el in menu_item_text:
+  print(el.get_text())
+'''
+
+menu_box = bs.find_all('nav', id="menu-box")
+
+# find : 조건과 일치하는 첫 번째 요소
+box = bs.find('div', id="box")
+# print(menu_box)
+print(box)
+
+
