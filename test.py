0cc6336cf5f457684e069c8f8e46bb836bdbf4bf
SangWon7242 <gotkdqja@gmail.com>
2024-03-05 21:21:21 +0900

    네이버 뉴스 제목 스크래핑 하기

--- test.py
+++ test.py
@@ -7,40 +7,36 @@ url = 'https://www.naver.com/'
 # GET 요청 보내기
 resp = requests.get(url)
 
-html = """
-<nav class="menu-box-1" id="menu-box">
-  <ul>
-    <li>
-      <a class="menu-item-text" href="https://www.naver.com">네이버로 이동</a>
-    </li>
-    <li>
-      <a class="menu-item-text" href="https://www.google.com">구글로 이동</a>
-    </li>
-    <li>
-      <a class="menu-item-text" href="https://www.daum.net">다음으로 이동</a>
-    </li>
-  </ul>
-</nav>
-<div id="box"></div>
-"""
+# 네이버 뉴스 url
+url = 'https://news.naver.com/section/103'
 
-# HTML 파싱
-bs = BeautifulSoup(html, 'html.parser')
+# resp : response의 약자
+# 지정 웹 사이트의 get요청을 보내서
+# requests를 이용해 웹 페이지의 html 문서 내용 가져오기
+resp = requests.get(url)
 
-# find_all : 조건과 일치하는 모든 요소의 리스트
-# 해당 엘리먼트의 클래스 이름으로 접근
-menu_item_text = bs.find_all('a', class_="menu-item-text")
+# 웹 페이지의 HTML 내용을 BeautifulSoup 객체로 변환
+bs = BeautifulSoup(resp.text, 'html.parser')
 
-'''
-for el in menu_item_text:
-  print(el.get_text())
-'''
+new_title = bs.find_all('a', class_='sa_text_title')
 
-menu_box = bs.find_all('nav', id="menu-box")
+new_title_list = []
 
-# find : 조건과 일치하는 첫 번째 요소
-box = bs.find('div', id="box")
-# print(menu_box)
-print(box)
+for title in new_title:
+  new_title_list.append(title.get_text())  
 
+# 뉴스 제목 안에 있던 \n 을 제거하는 코드    
+cleaned_news_tit = [item.replace('\n', '') for item in new_title_list]
 
+for idx, tit in enumerate(cleaned_news_tit):
+  print(f"{idx + 1} : {tit}")
+  
+# 해당 키워드로 리스트 순회 후 찾은 내용을 
+# keyword_tit_list 에 저장
+keyword = '봄'  
+keyword_tit_list = []
+for tit in cleaned_news_tit:
+  if keyword in tit:
+    keyword_tit_list.append(tit)
+
+print(keyword_tit_list)    
